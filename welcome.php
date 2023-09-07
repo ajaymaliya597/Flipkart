7057b4535397dd847145fd8a033b0fa4b90de8c3
ajaymaliya597 <116053948+ajaymaliya597@users.noreply.github.com>
2023-09-07 14:47:28 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,45 @@
+<?php
+include("connection.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flipkart-ECommerce Website</title>
+    <!-- style -->
+
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/util.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+    <link rel="stylesheet" href="css/login.css">
+
+    <!-- favicon -->
+
+    <link rel="shortcut icon" href="img/ico.png" type="image/x-icon" />
+</head>
+
+<body>
+    <div>
+
+        <div style="border: 12px solid black;
+    height: 50vh;
+    width: 47vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    top: 10vw;
+    left: 24vw;
+    font-size: 50px;
+     background: rgba(255, 255, 255, 0);
+  box-shadow: 0 0px 5px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(50.5px);
+  border: 2px solid rgba(255, 255, 255, 0.18);">
+            <h1>Welcome User</h1>
+        </div>
+    </div>
+</body>
+
+</html>
